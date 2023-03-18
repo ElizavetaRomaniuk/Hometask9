99c9a97e4fd978fb4c2ce9341d0eab8998fb2ea8
Elizaveta Romaniuk <elizavetasiniuk@gmail.com>
2023-03-18 13:01:35 +0300

    Task 2 done

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+static int Prompt(string message)
+{
+    System.Console.Write(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int RecursionSumOfPositiveInteger(int M, int N)
+{
+    if (M==N) return N;
+    else 
+    return N + RecursionSumOfPositiveInteger(M, N-1);
+}
+
+int N = Prompt("Input the highest number: ");
+int M = Prompt("Input the lowest number: ");
+System.Console.WriteLine($"Sum of the numbers = {RecursionSumOfPositiveInteger(M, N)}");
